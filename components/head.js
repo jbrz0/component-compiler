bc33ac50cd8a774974c8c3906be9eb7db709e49f
Justin <justinb@live.ca>
2020-09-03 20:10:25 -0400

    update meta tag for lg screen

--- components/head.js
+++ components/head.js
@@ -4,6 +4,7 @@ const head = (title) => {
 
   return `<head>
   <meta charset="utf-8">
+  <meta content=’width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0′ name=’viewport’ />
   <title>${title}</title>
   <link rel="stylesheet" href="style.css">
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600;900&family=Poppins&display=swap" rel="stylesheet">
